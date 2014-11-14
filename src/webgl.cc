d9f26c59cb9cf3601049e705e2564d6fc5e2c643
dan <dan@sawtoothideas.com>
2014-11-14 03:12:56 -0700

    Set depth bits to 32.

--- src/webgl.cc
+++ src/webgl.cc
@@ -51,7 +51,7 @@ WebGL::WebGL(int width, int height) :
       AGL_RGBA,
       AGL_DOUBLEBUFFER,
       AGL_PIXEL_SIZE, 32,
-      AGL_DEPTH_SIZE, 16,
+      AGL_DEPTH_SIZE, 32,
       AGL_ACCELERATED,
       AGL_NONE
     };
